8e8cb3a306efebd4782ef324744cc7ec207ac9ce
Sophie <48259684+alexsophiekim@users.noreply.github.com>
2020-06-24 12:59:23 +1200

    simple validate login

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import LoginForm from "../forms/LoginForm"
 
-const LoginPage = () => (
-    <div>
-        <h1>Login Page</h1>
-    </div>
-)
+class LoginPage extends React.Component {3
+    sumbit = data => {
+        console.log(data);
+    };
+    
+    render() {
+        return(
+            <div>
+                <h1>Login Page</h1>
+                 <LoginForm submit={this.submit}/>
+            </div>
+        );
+    }
+}
+
 
 export default LoginPage;
